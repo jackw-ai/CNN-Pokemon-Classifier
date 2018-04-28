0c955bf159b3d8a4790b05e356a4ca1d0df7116a
eutopi <tongyuzhou98@gmail.com>
2018-04-28 13:51:23 -0400

    separation of train vs test and usage of categorical label array improves accuracy to 15%

--- cnn.py
+++ cnn.py
@@ -12,19 +12,21 @@ classifier = Sequential()
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Flatten())
-classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+#classifier.add(Dense(units = 128, activation = 'relu'))
+#classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(8, input_dim=4, activation='relu'))
+classifier.add(Dense(18, activation='softmax'))
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
-train = 'type1_sorted'
-test = 'type1_sorted'
-training_set = train_datagen.flow_from_directory(train, target_size = (64, 64), batch_size = 32, class_mode = 'binary')
-test_set = test_datagen.flow_from_directory(test, target_size = (64, 64), batch_size = 32, class_mode = 'binary')
+train = 'type1_sorted/train'
+test = 'type1_sorted/test'
+training_set = train_datagen.flow_from_directory(train, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+test_set = test_datagen.flow_from_directory(test, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
 
-classifier.fit_generator(training_set, steps_per_epoch = 8000, epochs = 25, 
-                         validation_data = test_set, validation_steps = 2000)
+classifier.fit_generator(training_set, steps_per_epoch = 752, epochs = 25, 
+                         validation_data = test_set, validation_steps = 188)
 
 path = 'type1_sorted/Normal/128.png'
 test_image = image.load_img(path, target_size = (64, 64))
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import glob, shutil
-import os
-import re
+import os, re, sys
+import numpy
+from sklearn.model_selection import train_test_split
 from collections import namedtuple
 
 Pokemon = namedtuple('Pokemon', 'name, type1, type2')
@@ -14,18 +15,26 @@ def types (path):
         dict[info[0]] = Pokemon(info[1], info[2], info[3])
     return dict
 
-def sort (dict, path):
+def load (dict, path):
     src_dir = path
-    for pngfile in glob.iglob(os.path.join(src_dir, "*.png")):
+    dataset = list(glob.iglob(os.path.join(src_dir, "*.png")))
+    numpy.random.shuffle(dataset)
+    train, test = train_test_split(dataset, test_size = 0.2)
+    sort(dict, train, 'train')
+    sort(dict, test, 'test')
+
+def sort (dict, list, datatype):
+    for pngfile in list:
         name = str(pngfile).split('/')[2]
         id = ''.join(re.findall(r'\b\d+\b', name))
         if id != '':
             primary_type = dict[id].type1
-            dst_dir = 'type1_sorted/' + primary_type + '/' + name 
+            dst_dir = 'type1_sorted/' + datatype + '/' + primary_type + '/' + name 
             if not os.path.exists(os.path.dirname(dst_dir)):
                 os.makedirs(os.path.dirname(dst_dir))
             shutil.copy(pngfile, dst_dir)
 
 # divides the sprite images into their respective types
+shutil.rmtree('type1_sorted') # deletes the directory and reshuffles images each time 
 typing = types('data/Pokemon-2.csv')
-sort(typing, 'data/icons')
+load(typing, 'data/icons')
