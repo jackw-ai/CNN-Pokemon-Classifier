627d85b9058f49788bfb6735fb0da7e2474a542c
eutopi <tongyuzhou98@gmail.com>
2018-04-28 14:17:46 -0400

    fixed array shape error

--- preprocess.py
+++ preprocess.py
@@ -18,21 +18,30 @@ def types (path):
 def load (dict, path):
     src_dir = path
     dataset = list(glob.iglob(os.path.join(src_dir, "*.png")))
-    numpy.random.shuffle(dataset)
-    train, test = train_test_split(dataset, test_size = 0.2)
-    sort(dict, train, 'train')
-    sort(dict, test, 'test')
-
-def sort (dict, list, datatype):
-    for pngfile in list:
+    type_dict = {}
+    for pngfile in dataset:
         name = str(pngfile).split('/')[2]
         id = ''.join(re.findall(r'\b\d+\b', name))
         if id != '':
             primary_type = dict[id].type1
-            dst_dir = 'type1_sorted/' + datatype + '/' + primary_type + '/' + name 
-            if not os.path.exists(os.path.dirname(dst_dir)):
-                os.makedirs(os.path.dirname(dst_dir))
-            shutil.copy(pngfile, dst_dir)
+            if primary_type in type_dict:
+                type_dict[primary_type].append(pngfile)
+            else:
+                type_dict[primary_type] = [pngfile]
+    for key in type_dict:
+        values = type_dict[key]
+        numpy.random.shuffle(values)
+        train, test = train_test_split(values, test_size = 0.2)
+        sort(key, train, 'train')
+        sort(key, test, 'test')
+
+def sort (type, dataset, datatype):
+    for img in dataset:
+        name = str(img).split('/')[2]
+        dst_dir = 'type1_sorted/' + datatype + '/' + type + '/' + name 
+        if not os.path.exists(os.path.dirname(dst_dir)):
+            os.makedirs(os.path.dirname(dst_dir))
+        shutil.copy(img, dst_dir)
 
 # divides the sprite images into their respective types
 shutil.rmtree('type1_sorted') # deletes the directory and reshuffles images each time 
