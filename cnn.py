72a13110eb7bb2adc3f8914dec802b067058bb2f
wtingda <tw5@williams.edu>
2018-04-29 15:51:47 -0400

    added saving models for primary and secondary types

--- cnn.py
+++ cnn.py
@@ -13,35 +13,45 @@ from keras.models import model_from_json
 import numpy as np
 import os,random, sys
 
-classifier = Sequential()
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Flatten())
-#classifier.add(Dense(units = 128, activation = 'relu'))
-#classifier.add(Dense(units = 1, activation = 'sigmoid'))
-classifier.add(Dense(128, input_dim=4, activation='relu'))
-classifier.add(Dense(18, activation='softmax'))
-classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+def create_cnn(primary = True):
+    ''' builds a model for primary or secondary types and saves the model '''
+    classifier = Sequential()
+    classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+    classifier.add(MaxPooling2D(pool_size = (2, 2)))
+    classifier.add(Flatten())
+    #classifier.add(Dense(units = 128, activation = 'relu'))
+    #classifier.add(Dense(units = 1, activation = 'sigmoid'))
+    classifier.add(Dense(128, input_dim=4, activation='relu'))
 
-train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
-test_datagen = ImageDataGenerator(rescale = 1./255)
-train = 'type1_sorted/train'
-test = 'type1_sorted/test'
-training_set = train_datagen.flow_from_directory(train, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
-test_set = test_datagen.flow_from_directory(test, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+    categories = 18 if primary else 19
 
-classifier.fit_generator(training_set, steps_per_epoch = 752, epochs = 5, 
+    classifier.add(Dense(categories, activation='softmax'))
+    classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+    train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+    test_datagen = ImageDataGenerator(rescale = 1./255)
+
+    train = 'type1_sorted/train' if primary else 'type2_sorted/train' 
+    test = 'type1_sorted/test' if primary else 'type2_sorted/test' 
+
+    training_set = train_datagen.flow_from_directory(train, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+    test_set = test_datagen.flow_from_directory(test, target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+
+    classifier.fit_generator(training_set, steps_per_epoch = 752, epochs = 5, 
                          validation_data = test_set, validation_steps = 188)
 
-# serialize model to JSON
-classifier_json = classifier.to_json()
+    # save the classifier
+    classifier_json = classifier.to_json()
 
-if not os.path.exists(os.path.dirname("model/")):
-    os.makedirs(os.path.dirname("model/"))
+    if not os.path.exists(os.path.dirname("model/")):
+        os.makedirs(os.path.dirname("model/"))
+    filename = "classifier1" if primary else "classifier2"
+    with open("model/" + filename + ".json", "w") as json_file:
+        json_file.write(classifier_json)
 
-with open("model/classifier.json", "w") as json_file:
-    json_file.write(classifier_json)
+    # serialize weights to HDF5
+    classifier.save_weights("model/" + filename + ".h5")
+    print("Saved model to disk")
 
-# serialize weights to HDF5
-classifier.save_weights("model/classifier.h5")
-print("Saved model to disk")
+# build classifier for type 2
+create_cnn(False)
