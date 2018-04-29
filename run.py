94d7e36ebbc266cf6222929c1c79586303c9ced7
wtingda <tw5@williams.edu>
2018-04-29 16:11:57 -0400

    classifies both primary and secondary types

--- run.py
+++ run.py
@@ -11,34 +11,56 @@ from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import model_from_json
 import numpy as np
-import os,random, sys
+import os,random, sys, re
 
+from preprocess import types
+
+# for checking accuracy
+type_dict = types('data/Pokemon-2.csv')
+
+def get_type(filepath):
+    ''' returns the Pokemon tuple based on file path '''
+    filename = filepath.split('/')[-1]
+    id = ''.join(re.findall(r'\b\d+\b', filename))
+    return type_dict[id]
+    
 # load json and create model
-json_file = open('model/classifier.json', 'r')
+json_file = open('model/classifier1.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 classifier = model_from_json(loaded_model_json)
 # load weights into new model
-classifier.load_weights("model/classifier.h5")
-print("Loaded classifier from disk")
+classifier.load_weights("model/classifier1.h5")
+
+json_file = open('model/classifier2.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+classifier2 = model_from_json(loaded_model_json)
+# load weights into new model
+classifier2.load_weights("model/classifier2.h5")
+print("Loaded classifiers from disk")
 
 # tests
 test = 'type1_sorted/test'
 path = test
 path += '/' + random.choice([x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))])
-a = random.choice(os.listdir(path))
+imagepath = path + '/' + random.choice(os.listdir(path))
 
-test_image = image.load_img(path+'/'+a, target_size = (64, 64))
+test_image = image.load_img(imagepath, target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict_classes(test_image)
+result1 = classifier.predict_classes(test_image)
+result2 = classifier2.predict_classes(test_image)
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 test_set = test_datagen.flow_from_directory(test, target_size = (64, 64), batch_size = 32,
                                             class_mode = 'categorical')
+predicted_type = [type for type, index in test_set.class_indices.items() if index == result1[0]][0]
 
-# print(test_set.class_indices)
-# print(a)
-predicted_type = [type for type, index in test_set.class_indices.items() if index == result[0]][0]
-print("The actual type is " + path.split('/')[-1])
-print("The predicted type is " + predicted_type)
+test_set2 = test_datagen.flow_from_directory('type2_sorted/test', target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+predicted_type2 = [type for type, index in test_set2.class_indices.items() if index == result2[0]][0]
+
+pokemon = get_type(imagepath)
+second = "" if pokemon.type2 == 'None' else " and type " + pokemon.type2
+print("The Pokemon " + pokemon.name + " has type " + pokemon.type1 + second)
+print("The predicted type is " + predicted_type + " and " + predicted_type2)
