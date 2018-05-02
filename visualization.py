3170404a96d0bb8bd35e232d1ba9ece3d0cb322d
wtingda <tw5@williams.edu>
2018-05-01 21:08:20 -0400

    add loss plots

--- visualization.py
+++ visualization.py
@@ -64,6 +64,28 @@ def plot_game(game, pokemons = random.sample(range(1, 152), 10)):
     plt.suptitle("Pokemons from " + str(game))
     plt.show()
 
+def plot_loss(history):
+    # loss
+    plt.figure(figsize = (8, 6))
+    plt.plot(history.history['loss'], 'r', linewidth = 3.0)
+    plt.plot(history.history['val_loss'], 'b', linewidth = 3.0)
+    plt.legend(['Training loss', 'Validation Loss'], fontsize = 18)
+    plt.xlabel('Epochs ', fontsize = 16)
+    plt.ylabel('Loss', fontsize = 16)
+    plt.title('Loss Curves', fontsize = 16)
+ 
+    # accuracy
+    plt.figure(figsize = (8, 6])
+    plt.plot(history.history['acc'], 'r', linewidth = 3.0)
+    plt.plot(history.history['val_acc'], 'b', linewidth = 3.0)
+    plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize = 18)
+    plt.xlabel('Epochs ', fontsize = 16)
+    plt.ylabel('Accuracy', fontsize = 16)
+    plt.title('Accuracy Curves',fontsize = 16)
+
+    plt.suptitle('Loss & Accuracy')
+    plt.show()
+    
 if __name__ == "__main__":
     plot_pokemon(150)
     plot_game("platinum")
