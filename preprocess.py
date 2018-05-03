a9afd9a21419a48244eb5605ba6581533a3a9aef
eutopi <tongyuzhou98@gmail.com>
2018-05-03 10:49:13 -0400

    resized images

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,11 @@
 import glob, shutil
 import os, re, sys
 import numpy
+import matplotlib.image as mpimg
+from skimage import color, filters, measure, morphology, transform
+from scipy import ndimage
+from functools import reduce
+from math import ceil, floor
 from sklearn.model_selection import train_test_split
 from collections import namedtuple
 
@@ -64,6 +69,101 @@ def load(dict, path, primary = True, testsize = 0.2):
         sort(key, train, 'train', primary)
         sort(key, test, 'test', primary)
 
+# box modification code by hemagso
+def bbox_reducer(a,b):
+    """Reduces two bounding boxes tuples to a bounding box
+    encompassing both. The bounding box format expected is
+    
+    (min_row, min_col, max_row, max_col)
+    
+    Used with the reduce function to merge bounding boxes
+    
+    """
+    min_row = min(a[0],b[0])
+    min_col = min(a[1],b[1])
+    max_row = max(a[2],b[2])
+    max_col = max(a[3],b[3])
+    return (min_row, min_col, max_row, max_col)
+
+# optimization code by hemagso
+def optimize(image,new_size = (64,64),plot=False,square=True, id = None):
+    image_bw = color.rgb2gray(image)
+    image_countour = filters.sobel(image_bw)
+    image_filled = ndimage.binary_fill_holes(image_countour)
+    
+    image_mask = morphology.convex_hull_image(image_filled)
+    
+    labels, n_objects = ndimage.label(image_mask)
+    regions = measure.regionprops(labels)
+    slices = ndimage.find_objects(labels)
+
+    #Get border
+    bbox_list = [r.bbox for r in regions]
+    min_row, min_col, max_row, max_col = reduce(bbox_reducer,bbox_list)
+    
+    #If the bounding box is not squared, make it so
+    if square:
+        len_row = max_row - min_row
+        len_col = max_col - min_col
+
+        if len_row > len_col:
+            min_col -= ceil((len_row - len_col)/2)
+            max_col += floor((len_row - len_col)/2)
+        else:
+            min_row -= ceil((len_col - len_row)/2)
+            max_row += floor((len_col - len_row)/2)      
+    
+
+    #We may have some out of bound stuff hapenning here
+#    if (max_row - min_row) > image.shape[0]:
+#        raise ValueError("ID = {id} - Bounding box height is greater than image height".format(id=id))
+#    if (max_col - min_col) > image.shape[1]:
+#        raise ValueError("ID = {id} - Bounding box width is greater than image width".format(id=id))
+
+    #If Bounding box exceeds image limits, we shift it inside
+    if min_row < 0:
+        max_row += abs(min_row)
+        min_row = 0
+    if min_col < 0:
+        max_col += abs(min_col)
+        min_col = 0
+    if max_row >= image.shape[1]:
+        min_row -= max_row - image.shape[1] + 1
+        max_row = image.shape[0]-1
+    if max_col >= image.shape[1]:
+        min_col -= max_col - image.shape[1] + 1
+        max_col = image.shape[1]-1    
+    
+    image_slice = (
+        slice(min_row,max_row,None) ,
+        slice(min_col,max_col,None)               
+    )
+    
+    image_bounded = image[image_slice]
+    
+    image_resize = transform.resize(image_bounded,new_size)
+    
+    if plot:
+        image_box = patches.Rectangle(
+            (min_col,min_row),
+            max_row - min_row,  
+            max_col - min_col,  
+            fc = "none",
+            ec = "red"
+        )
+        img_arr = [
+            (image,None,None),
+            (image_bw,"gray",None),
+            (image_countour,"gray",None),
+            (image_filled,"gray",None),
+            (image_mask,"gray",None),
+            (image,None,image_box),
+            (image_resize,None,None)
+        ]
+        plot_intermediate_steps(img_arr)
+    
+    return image_resize    
+
 def sort(type, dataset, datatype, primary = True):
     '''
     divide the pokemon into folders corresponding to their type 
@@ -73,6 +173,7 @@ def sort(type, dataset, datatype, primary = True):
     '''
     
     folder = 'type1_sorted/' if primary else 'type2_sorted/'
+
         
     for img in dataset:
         name = str(img).split('/')[-1]
@@ -82,7 +183,9 @@ def sort(type, dataset, datatype, primary = True):
         if not os.path.exists(os.path.dirname(dst_dir)):
             os.makedirs(os.path.dirname(dst_dir))
         shutil.copy(img, dst_dir)
-
+        img_file = mpimg.imread(dst_dir)
+        img_file = optimize(img_file)
+        mpimg.imsave(dst_dir, img_file)
 
 if __name__ == "__main__":        
     # divides the sprite images into their respective types
